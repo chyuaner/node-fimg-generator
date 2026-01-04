8cb0ae34700fecf70a4b0ceb35fa362ff754e703
Yuan Chiu <chyuaner@gmail.com>
2026-01-04 09:19:41 +0800

    feat: add gnome backgrounds

--- src/core/loaders/assets.ts
+++ src/core/loaders/assets.ts
@@ -13,6 +13,16 @@ export const pathMap: Record<string, string> = {
     'morphogenesis-l': '/background/morphogenesis-l.svg',
     'symbolic-d': '/background/symbolic-d.png',
     'symbolic-l': '/background/symbolic-l.png',
+    'curves': '/background/curves.png',
+    'lava': '/background/lava.png',
+    'scie': '/background/scie.png',
+    'scie-alt': '/background/scie-alt.png',
+    'spaceflare': '/background/spaceflare.png',
+    'spring': '/background/spring.png',
+    'stripes': '/background/stripes.png',
+    'terraform': '/background/terraform.png',
+    'terraform-orange': '/background/terraform-orange.png',
+    'waves': '/background/waves.png'
 }
 
 // 以 function 形式導出，並註明回傳型別
